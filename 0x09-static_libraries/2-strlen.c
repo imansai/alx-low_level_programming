de1369358614cec1ab55a67c79fc1208176a4e5f
imansai <im.saibari@gmail.com>
2023-04-10 17:42:58 -0700

    First try

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen - gives the length of a string
+ * @s: string
+ * Return: string length
+ */
+int _strlen(char *s)
+{
+	int longi = 0;
+
+	while (*s != '\0')
+	{
+		longi++;
+		s++;
+	}
+
+	return (longi);
+}
